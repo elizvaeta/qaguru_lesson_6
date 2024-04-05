cf6b2492d45726536353ac68966f3e4b3295968d
Elizaveta Kazova <e.a.kazova@gmail.com>
2024-04-06 00:40:47 +0500

    Исправлена проверка времени; убраны неиспользуемые переменные

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -8,9 +8,7 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
-
-    if time(hour=22) >= current_time > time(hour=6):
+    if time(hour=22) > current_time >= time(hour=6):
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -31,12 +29,10 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
-
     if dark_theme_enabled_by_user is not None:
         is_dark_theme = dark_theme_enabled_by_user
     else:
-        if time(hour=22) >= current_time > time(hour=6):
+        if time(hour=22) > current_time >= time(hour=6):
             is_dark_theme = False
         else:
             is_dark_theme = True
